0ce8f4941dd6f82d8e2474b00dcd25ad5e56e35f
emirceyani <emirceyani@gmail.com>
2023-06-28 14:31:55 -0700

    Upload Dropout-GNN Models

--- src/client.py
+++ src/client.py
@@ -10,6 +10,7 @@ class Client_GC():
         self.train_size = train_size
         self.dataLoader = dataLoader
         self.optimizer = optimizer
+        self.dropout = args.dropout
         self.args = args
 
         self.W = {key: value for key, value in self.model.named_parameters()}
@@ -39,7 +40,7 @@ class Client_GC():
 
     def local_train(self, local_epoch):
         """ For self-train & FedAvg """
-        train_stats = train_gc(self.model, self.name, self.dataLoader, self.optimizer, local_epoch, self.args.device)
+        train_stats = train_gc(self.model, self.name, self.dataLoader, self.dropout, self.optimizer, local_epoch, self.args.device)
 
         self.train_stats = train_stats
         self.weightsNorm = torch.norm(flatten(self.W)).item()
@@ -82,7 +83,7 @@ class Client_GC():
 
     def local_train_prox(self, local_epoch, mu):
         """ For FedProx """
-        train_stats = train_gc_prox(self.model, self.dataLoader, self.optimizer, local_epoch, self.args.device,
+        train_stats = train_gc_prox(self.model, self.dataLoader, self.dropout, self.optimizer, local_epoch, self.args.device,
                                self.gconvNames, self.W, mu, self.W_old)
 
         self.train_stats = train_stats
@@ -100,7 +101,6 @@ class Client_GC():
     def evaluate_prox(self, mu):
         return eval_gc_prox(self.model, self.dataLoader['test'], self.args.device, self.gconvNames, mu, self.W_old)
 
-
 def copy(target, source, keys):
     for name in keys:
         target[name].data = source[name].data.clone()
@@ -117,31 +117,21 @@ def calc_gradsNorm(gconvNames, Ws):
     convGradsNorm = torch.norm(flatten(grads_conv)).item()
     return convGradsNorm
 
-def train_gc(model, name, dataloaders, optimizer, local_epoch, device):
+def train_gc(model, name, dataloaders, dropout, optimizer, local_epoch, device):
     losses_train, accs_train, losses_val, accs_val, losses_test, accs_test = [], [], [], [], [], []
     train_loader, val_loader, test_loader = dataloaders['train'], dataloaders['val'], dataloaders['test']
     for epoch in range(local_epoch):
         model.train()
 
-        total_loss = 0.
-        ngraphs = 0
-
-        acc_sum = 0
-
-        for  batch in train_loader:
+        for batch in train_loader:
             optimizer.zero_grad()
             batch.to(device)
-            pred = model(batch)
-            label = batch.y
-            acc_sum += pred.max(dim=1)[1].eq(label).sum().item()
-            loss = model.loss(pred, label)
+            pred = model(batch.x, batch.edge_index, dropout)
+            loss = model.loss(pred[batch.train_mask], batch.y[batch.train_mask])
             loss.backward()
             optimizer.step()
-            total_loss += loss.item() * batch.num_graphs
-            ngraphs += batch.num_graphs
-        total_loss /= ngraphs
-        acc = acc_sum / ngraphs
 
+        total_loss, acc = eval_gc(model, train_loader, device)
         loss_v, acc_v = eval_gc(model, val_loader, device)
         loss_tt, acc_tt = eval_gc(model, test_loader, device)
 
@@ -161,62 +151,23 @@ def train_gc(model, name, dataloaders, optimizer, local_epoch, device):
     return {'trainingLosses': losses_train, 'trainingAccs': accs_train, 'valLosses': losses_val, 'valAccs': accs_val,
             'testLosses': losses_test, 'testAccs': accs_test}
 
-def train_gc_pe(model, dataloaders, optimizer, local_epoch, device):
-    losses_train, accs_train, losses_val, accs_val, losses_test, accs_test = [], [], [], [], [], []
-    train_loader, val_loader, test_loader = dataloaders['train'], dataloaders['val'], dataloaders['test']
-    for epoch in range(local_epoch):
-        model.train()
-        total_loss = 0.
-        ngraphs = 0
-
-        acc_sum = 0
-
-        for _, batch in enumerate(train_loader):
-            batch.x = torch.ones_like(batch.x)
-            batch.to(device)
-            optimizer.zero_grad()
-            pred = model(batch)
-            label = batch.y
-            acc_sum += pred.max(dim=1)[1].eq(label).sum().item()
-            loss = model.loss(pred, label)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item() * batch.num_graphs
-            ngraphs += batch.num_graphs
-        total_loss /= ngraphs
-        acc = acc_sum / ngraphs
-
-        loss_v, acc_v = eval_gc(model, val_loader, device)
-        loss_tt, acc_tt = eval_gc(model, test_loader, device)
-
-        losses_train.append(total_loss)
-        accs_train.append(acc)
-        losses_val.append(loss_v)
-        accs_val.append(acc_v)
-        losses_test.append(loss_tt)
-        accs_test.append(acc_tt)
-
-    return {'trainingLosses': losses_train, 'trainingAccs': accs_train, 'valLosses': losses_val, 'valAccs': accs_val,
-            'testLosses': losses_test, 'testAccs': accs_test}
-
+@torch.no_grad()
 def eval_gc(model, test_loader, device):
     model.eval()
 
     total_loss = 0.
     acc_sum = 0.
-    ngraphs = 0
+    nnodes = 0
     for batch in test_loader:
         batch.to(device)
-        with torch.no_grad():
-            pred = model(batch)
-            label = batch.y
-            loss = model.loss(pred, label)
-        total_loss += loss.item() * batch.num_graphs
-        acc_sum += pred.max(dim=1)[1].eq(label).sum().item()
-        ngraphs += batch.num_graphs
+        pred = model(batch.x, batch.edge_index)
+        label = batch.y
+        loss = model.loss(pred[batch.train_mask], label[batch.train_mask])
+        total_loss += loss.item() 
+        acc_sum += int(pred[batch.train_mask].eq(label[batch.train_mask]).sum().item())
+        nnodes += int(batch.test_mask.sum())
 
-    return total_loss/ngraphs, acc_sum/ngraphs
-
+    return total_loss/nnodes, acc_sum/nnodes
 
 def _prox_term(model, gconvNames, Wt):
     prox = torch.tensor(0., requires_grad=True)
@@ -226,31 +177,23 @@ def _prox_term(model, gconvNames, Wt):
             prox = prox + torch.norm(param - Wt[name]).pow(2)
     return prox
 
-def train_gc_prox(model, dataloaders, optimizer, local_epoch, device, gconvNames, Ws, mu, Wt):
+def train_gc_prox(model, dataloaders, dropout, optimizer, local_epoch, device, gconvNames, Ws, mu, Wt):
     losses_train, accs_train, losses_val, accs_val, losses_test, accs_test = [], [], [], [], [], []
     convGradsNorm = []
     train_loader, val_loader, test_loader = dataloaders['train'], dataloaders['val'], dataloaders['test']
     for epoch in range(local_epoch):
         model.train()
-        total_loss = 0.
-        ngraphs = 0
-
-        acc_sum = 0
 
         for _, batch in enumerate(train_loader):
             batch.to(device)
             optimizer.zero_grad()
-            pred = model(batch)
+            pred = model(batch.x, batch.edge_index, dropout)
             label = batch.y
-            acc_sum += pred.max(dim=1)[1].eq(label).sum().item()
-            loss = model.loss(pred, label) + mu / 2. * _prox_term(model, gconvNames, Wt)
+            loss = model.loss(pred[batch.train_mask], label[batch.train_mask]) + mu / 2. * _prox_term(model, gconvNames, Wt)
             loss.backward()
             optimizer.step()
-            total_loss += loss.item() * batch.num_graphs
-            ngraphs += batch.num_graphs
-        total_loss /= ngraphs
-        acc = acc_sum / ngraphs
 
+        total_loss, acc = eval_gc(model, train_loader, device)
         loss_v, acc_v = eval_gc(model, val_loader, device)
         loss_tt, acc_tt = eval_gc(model, test_loader, device)
 
@@ -266,20 +209,20 @@ def train_gc_prox(model, dataloaders, optimizer, local_epoch, device, gconvNames
     return {'trainingLosses': losses_train, 'trainingAccs': accs_train, 'valLosses': losses_val, 'valAccs': accs_val,
             'testLosses': losses_test, 'testAccs': accs_test, 'convGradsNorm': convGradsNorm}
 
+@torch.no_grad()
 def eval_gc_prox(model, test_loader, device, gconvNames, mu, Wt):
     model.eval()
 
     total_loss = 0.
     acc_sum = 0.
-    ngraphs = 0
+    nnodes = 0
     for batch in test_loader:
         batch.to(device)
-        with torch.no_grad():
-            pred = model(batch)
-            label = batch.y
-            loss = model.loss(pred, label) + mu / 2. * _prox_term(model, gconvNames, Wt)
-        total_loss += loss.item() * batch.num_graphs
-        acc_sum += pred.max(dim=1)[1].eq(label).sum().item()
-        ngraphs += batch.num_graphs
+        pred = model(batch.x, batch.edge_index)
+        label = batch.y
+        loss = model.loss(pred[batch.train_mask], label[batch.train_mask]) + mu / 2. * _prox_term(model, gconvNames, Wt)
+        total_loss += loss.item() 
+        acc_sum += int(pred[batch.train_mask].eq(label[batch.train_mask]).sum().item())
+        nnodes += int(batch.test_mask.sum())
 
-    return total_loss/ngraphs, acc_sum/ngraphs
+    return total_loss/nnodes, acc_sum/nnodes
--- src/models.py
+++ src/models.py
@@ -1,7 +1,36 @@
 import torch
 import torch.nn.functional as F
+from torch import Tensor
+from torch_sparse import SparseTensor, set_diag
+from torch_geometric.typing import Adj
 from torch_geometric.nn import GCNConv, global_add_pool, SAGEConv, GATConv
+from torch_geometric.utils import remove_self_loops, add_self_loops, degree
 import random
+
+
+"""
+Usified DropBlock Module for Dropout, DropEdge, and DropNode
+"""
+class DropBlock:
+    def __init__(self, dropping_method: str):
+        super(DropBlock, self).__init__()
+        self.dropping_method = dropping_method
+
+    def drop(self, x: Tensor, edge_index: Adj, drop_rate: float = 0):
+        if self.dropping_method == 'DropNode':
+            x = x * torch.bernoulli(torch.ones(x.size(0), 1) - drop_rate).to(x.device)
+            x = x / (1 - drop_rate)
+        elif self.dropping_method == 'DropEdge':
+            edge_reserved_size = int(edge_index.size(1) * (1 - drop_rate))
+            if isinstance(edge_index, SparseTensor):
+                row, col, _ = edge_index.coo()
+                edge_index = torch.stack((row, col))
+            perm = torch.randperm(edge_index.size(1))
+            edge_index = edge_index.t()[perm][:edge_reserved_size].t()
+        elif self.dropping_method == 'Dropout':
+            x = F.dropout(x, drop_rate)
+
+        return x, edge_index
 
 
 """
@@ -35,28 +64,36 @@ class serverGCN(torch.nn.Module):
 Client Models
 """
 class GAT(torch.nn.Module):
-    def __init__(self, in_channels, out_channels, dropout, heads=2):
+    def __init__(self, in_channels, out_channels, dropping_method: str = "DropEdge", heads=2):
         super(GAT, self).__init__()
-        self.dropout = dropout
+        self.dropping_method = dropping_method
+        self.drop_block = DropBlock(dropping_method)
         self.conv1 = GATConv(in_channels, heads, heads=heads)
         # On the Pubmed dataset, use heads=8 in conv2.
         self.conv2 = GATConv(heads * heads, out_channels, heads=1, concat=False, dropout=0.6)
         self.nclass = out_channels
 
-    def forward(self, inp):
-        x = F.elu(self.conv1(inp.x, inp.edge_index))
-        x = F.dropout(x, p=self.dropout, training=self.training)
-        x = self.conv2(x, inp.edge_index)
+    def forward(self, x: Tensor, edge_index: Adj, drop_rate: float = 0):
+
+        if self.training:
+            x, edge_index = self.drop_block.drop(x, edge_index, drop_rate)
+
+        x = F.elu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
         return F.log_softmax(x, dim=-1)
 
     def loss(self, pred, label):
         return F.nll_loss(pred, label)
 
 class GCN(torch.nn.Module):
-    def __init__(self, nfeat, nhid, nclass, nlayer, dropout):
+    def __init__(self, nfeat, nhid, nclass, nlayer, dropping_method: str = "DropEdge"):
         super(GCN, self).__init__()
+        self.dropping_method = dropping_method
+        self.drop_block = DropBlock(dropping_method)
+        self.edge_weight = None
+
+        self.transform_first = transform_first
         self.num_layers = nlayer
-        self.dropout = dropout
         self.nclass = nclass
 
         self.pre = torch.nn.Sequential(torch.nn.Linear(nfeat, nhid))
@@ -68,13 +105,13 @@ class GCN(torch.nn.Module):
 
         self.post = torch.nn.Sequential(torch.nn.Linear(nhid, nclass))
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+    def forward(self, x: Tensor, edge_index: Adj, drop_rate: float = 0):
+        if self.training:
+            x, edge_index = self.drop_block.drop(x, edge_index, drop_rate)
         x = self.pre(x)
         for i in range(len(self.graph_convs)):
             x = self.graph_convs[i](x, edge_index)
             x = F.relu(x)
-            x = F.dropout(x, self.dropout, training=self.training)
         x = self.post(x)
         return F.log_softmax(x, dim=1)
 
@@ -82,10 +119,11 @@ class GCN(torch.nn.Module):
         return F.nll_loss(pred, label)
 
 class SAGE(torch.nn.Module):
-    def __init__(self, nfeat, nhid, nclass, nlayer, dropout):
+    def __init__(self, nfeat, nhid, nclass, nlayer, dropping_method: str = "DropEdge"):
         super(SAGE, self).__init__()
+        self.dropping_method = dropping_method
+        self.drop_block = DropBlock(dropping_method)
         self.num_layers = nlayer
-        self.dropout = dropout
         self.nclass = nclass
 
         self.pre = torch.nn.Sequential(torch.nn.Linear(nfeat, nhid))
@@ -97,13 +135,13 @@ class SAGE(torch.nn.Module):
 
         self.post = torch.nn.Sequential(torch.nn.Linear(nhid, nclass))
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+    def forward(self, x: Tensor, edge_index: Adj, drop_rate: float = 0):
+        if self.training:
+            x, edge_index = self.drop_block.drop(x, edge_index, drop_rate)
         x = self.pre(x)
         for i in range(len(self.graph_convs)):
             x = self.graph_convs[i](x, edge_index)
             x = F.relu(x)
-            x = F.dropout(x, self.dropout, training=self.training)
         x = self.post(x)
         return F.log_softmax(x, dim=1)
 
