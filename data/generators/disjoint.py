743b092bbdb00c9c8ecd3192660bd6c0ebbb164a
emirceyani <emirceyani@gmail.com>
2023-10-26 15:06:41 -0700

    Add Louvain partitioning

--- data/generators/disjoint.py
+++ data/generators/disjoint.py
@@ -8,48 +8,71 @@ import torch_geometric
 from torch_geometric.data import Data
 from torch_geometric.utils import to_dense_adj, dense_to_sparse
 
-from utils import get_data, split_train, torch_save
+from utils import get_data, split_train, torch_save , louvain_graph_cut
 
+dset= 'Cora'
 data_path = '../../../datasets/'
-ratio_train = 0.2
-seed = 1234
-clients = [5, 10, 20]
+ratio_train = 0.6
+seed = 2021
+clients = [3, 5, 10, 20]
 
 random.seed(seed)
 np.random.seed(seed)
 torch.manual_seed(seed)
 
-def generate_data(dataset, n_clients):
-    # data = split_train(, dataset, data_path, ratio_train, 'disjoint', n_clients)
-    split_subgraphs(n_clients, get_data(dataset, data_path), dataset)
+def generate_data(dataset, n_clients, partition, mode = False):
+    if mode:
+        #Natural split then partition (apply partition to each split) - ours 
+        split_subgraphs_v1(n_clients,  get_data(dataset, data_path), dataset, partition)
+    else:
+        #Manual split then partition - FedPUB
+        data = split_train(get_data(dataset, data_path), dataset, data_path, ratio_train, 'disjoint', n_clients)
+        split_subgraphs_v2(n_clients,data, dataset, partition)
 
-def split_subgraphs(n_clients, data, dataset):
+def split_subgraphs_v1(n_clients, data, dataset, partition = "METIS"):
+    """
+    This function supports Natural splits then Graph partitioning
+    """
 
-    #Get the natural splits from Cora 
     tr_graph = data.subgraph(data.train_mask) 
     val_graph = data.subgraph(data.val_mask)
     tst_graph = data.subgraph(data.test_mask)
 
-    #Metis partitioning for each split
     G_tr = torch_geometric.utils.to_networkx(tr_graph)
     G_val = torch_geometric.utils.to_networkx(val_graph)
     G_tst = torch_geometric.utils.to_networkx(tst_graph)
-    n_cuts_tr, membership_tr = metis.part_graph(G_tr, n_clients)
-    n_cuts_val, membership_val = metis.part_graph(G_val, n_clients)
-    n_cuts_tst, membership_tst = metis.part_graph(G_tst, n_clients)
+
+    if partition == "METIS":
+        #Metis partitioning for each split
+        n_cuts_tr, membership_tr = metis.part_graph(G_tr, n_clients)
+        n_cuts_val, membership_val = metis.part_graph(G_val, n_clients)
+        n_cuts_tst, membership_tst = metis.part_graph(G_tst, n_clients)
+    elif partition == "Louvain":
+        n_cuts_tr, membership_tr = louvain_graph_cut(G_tr, map(str, tr_graph.y.tolist()), n_clients)
+        n_cuts_val, membership_val = louvain_graph_cut(G_val, map(str, val_graph.y.tolist()), n_clients)
+        n_cuts_tst, membership_tst = louvain_graph_cut(G_tst, map(str, tst_graph.y.tolist()), n_clients)
+    else:
+        print("Not implemented yet")
+
+    
     assert len(list(set(membership_tr))) == n_clients
-    print(f'Training graph partitioning done, metis, n_partitions: {len(list(set(membership_tr)))}, n_lost_edges: {n_cuts_tr}')
+    print(f'Training graph partitioning done,{partition}, n_partitions: {len(list(set(membership_tr)))}, n_lost_edges: {n_cuts_tr}')
     assert len(list(set(membership_val))) == n_clients
-    print(f'Validation graph partitioning done, metis, n_partitions: {len(list(set(membership_val)))}, n_lost_edges: {n_cuts_val}')
+    print(f'Validation graph partitioning done,{partition},n_partitions: {len(list(set(membership_val)))}, n_lost_edges: {n_cuts_val}')
     assert len(list(set(membership_tst))) == n_clients
-    print(f'Test graph partitioning done, metis, n_partitions: {len(list(set(membership_tst)))}, n_lost_edges: {n_cuts_tst}')
+    print(f'Test graph partitioning done,{partition},n_partitions: {len(list(set(membership_tst)))}, n_lost_edges: {n_cuts_tst}')
         
     adj = to_dense_adj(data.edge_index)[0]
     for client_id in range(n_clients):
         #Fetch client indices and nodes
-        client_indices_tr = list(np.where(np.array(membership_tr) == client_id)[0])
-        client_indices_val = list(np.where(np.array(membership_val) == client_id)[0])
-        client_indices_tst = list(np.where(np.array(membership_tst) == client_id)[0])
+        if partition == "METIS":    
+            client_indices_tr = list(np.where(np.array(membership_tr) == client_id)[0])
+            client_indices_val = list(np.where(np.array(membership_val) == client_id)[0])
+            client_indices_tst = list(np.where(np.array(membership_tst) == client_id)[0])
+        elif partition == "Louvain":
+            client_indices_tr = membership_tr[client_id]
+            client_indices_val = membership_val[client_id]
+            client_indices_tst = membership_tst[client_id]
 
         #Fetch edge indices and adjacency matrix 
         client_edge_index_tr, client_edge_index_val, client_edge_index_tst = [],[],[]
@@ -81,7 +104,7 @@ def split_subgraphs(n_clients, data, dataset):
         )
         assert tst_x.size(dim=0) > 0
 
-        torch_save(f'{data_path}{dataset}_disjoint/{n_clients}/', f'partition_{client_id}.pt', {
+        torch_save(f'{data_path}{dataset}_disjoint_v1_{partition}/{n_clients}/', f'partition_{client_id}.pt', {
             'client_tr': tr_data,
             'client_val': val_data,
             'client_tst': tst_data,
@@ -91,5 +114,66 @@ def split_subgraphs(n_clients, data, dataset):
         print(f'client_id: {client_id}, iid, n_val_nodes: {len(client_indices_val)}, n_val_edges: {cli_num_edges_val}')
         print(f'client_id: {client_id}, iid, n_test_nodes: {len(client_indices_tst)}, n_test_edges: {cli_num_edges_tst}')
 
-for n_clients in clients:
-    generate_data(dataset='Photo', n_clients=n_clients)
+def split_subgraphs_v2(n_clients, data, dataset, partition = "METIS"):
+
+    """
+    This function supports manual splits then Graph partitioning case
+    """
+    
+    G = torch_geometric.utils.to_networkx(data, to_undirected = True )
+    #Graph partitioning
+    if partition == "METIS":
+        n_cuts, membership = metis.part_graph(G, n_clients)
+        assert len(list(set(membership))) == n_clients
+    elif partition == "Louvain":
+        if n_clients >5:
+            n_cuts, membership = louvain_graph_cut(G, list(map(str, data.y.tolist())) , n_clients, delta = 80)
+        if n_clients >10:
+            n_cuts, membership = louvain_graph_cut(G, list(map(str, data.y.tolist())) , n_clients, delta = 120)
+        else:
+            n_cuts, membership = louvain_graph_cut(G, list(map(str, data.y.tolist())) , n_clients)
+    print(f'graph partition done, {partition}, n_partitions: {len(list(set(membership)))}, n_lost_edges: {n_cuts}')
+            
+    adj = to_dense_adj(data.edge_index)[0]
+    for client_id in range(n_clients):
+        if partition == "METIS":
+            client_indices = np.where(np.array(membership) == client_id)[0]
+        elif partition =="Louvain":
+            client_indices = membership[client_id]
+        client_indices = list(client_indices)
+        client_num_nodes = len(client_indices)
+
+        client_edge_index = []
+        client_adj = adj[client_indices][:, client_indices]
+        client_edge_index, _ = dense_to_sparse(client_adj)
+        client_edge_index = client_edge_index.T.tolist()
+        client_num_edges = len(client_edge_index)
+
+        client_edge_index = torch.tensor(client_edge_index, dtype=torch.long)
+        client_x = data.x[client_indices]
+        client_y = data.y[client_indices]
+        client_train_mask = data.train_mask[client_indices]
+        client_val_mask = data.val_mask[client_indices]
+        client_test_mask = data.test_mask[client_indices]
+  
+        client_data = Data(
+            x = client_x,
+            y = client_y,
+            edge_index = client_edge_index.t().contiguous(),
+            train_mask = client_train_mask,
+            val_mask = client_val_mask,
+            test_mask = client_test_mask
+        )
+        assert torch.sum(client_train_mask).item() > 0
+
+   
+        torch_save(f'{data_path}{dataset}_disjoint_v2_{partition}/{n_clients}/', f'partition_{client_id}.pt', {
+            'client_data': client_data,
+            'client_id': client_id
+        })
+        print(f'client_id: {client_id} , {partition}, n_train_node: {client_num_nodes}, n_train_edge: {client_num_edges}')
+
+for mode in [False]:
+    for part in [ "Louvain"]:      
+        for n_clients in clients:
+            generate_data(dataset=dset, n_clients=n_clients, partition=part, mode = mode)
--- data/generators/utils.py
+++ data/generators/utils.py
@@ -1,14 +1,19 @@
 import os
+import copy
 import torch
-
+import numpy as np
+import networkx as nx
+import community as community_louvain
+from sklearn import preprocessing
 import torch_geometric.datasets as datasets
 import torch_geometric.transforms as T
 
-from torch_geometric.data import Data
+from torch_geometric.data import Data, DataLoader
+from torch_geometric.utils import from_networkx
 from torch_geometric.transforms import BaseTransform
 from torch_geometric.utils import to_scipy_sparse_matrix
 
-from ogb.nodeproppred import PygNodePropPredDataset
+from ogb.nodeproppred import PygNodePropPredDataset 
 
 def torch_save(base_dir, filename, data):
     os.makedirs(base_dir, exist_ok=True)
@@ -53,6 +58,69 @@ def split_train(data, dataset, data_path, ratio_train, mode, n_clients):
     torch_save(f'{data_path}/{dataset}_{mode}/{n_clients}/','val.pt', {'data': data})
     print(f'Split done, n_train: {n_train}, n_test: {n_test}, n_val: {len(val_indices)}')
     return data
+
+
+def louvain_graph_cut(whole_graph, node_subjects, num_owners, delta = 20):
+    edges = whole_graph.edges
+    edge_index = torch.tensor(list(edges), dtype=torch.long).t().contiguous()
+    
+    G = nx.Graph()
+    G.add_edges_from(edges)
+
+    partition = community_louvain.best_partition(G)
+
+    groups = []
+
+    for key in partition.keys():
+        if partition[key] not in groups:
+            groups.append(partition[key])
+
+    partition_groups = {group_i: [] for group_i in groups}
+
+    for key in partition.keys():
+        partition_groups[partition[key]].append(key)
+
+    group_len_max = len(list(whole_graph.nodes())) // num_owners - delta
+    for group_i in groups:
+        while len(partition_groups[group_i]) > group_len_max:
+            long_group = list.copy(partition_groups[group_i])
+            partition_groups[group_i] = list.copy(long_group[:group_len_max])
+            new_grp_i = max(groups) + 1
+            groups.append(new_grp_i)
+            partition_groups[new_grp_i] = long_group[group_len_max:]
+
+    len_list = []
+    for group_i in groups:
+        len_list.append(len(partition_groups[group_i]))
+
+    len_dict = {}
+
+    for i in range(len(groups)):
+        len_dict[groups[i]] = len_list[i]
+    sort_len_dict = {k: v for k, v in sorted(len_dict.items(), key=lambda item: item[1], reverse=True)}
+
+    owner_node_ids = {owner_id: [] for owner_id in range(num_owners)}
+
+    owner_nodes_len = len(list(G.nodes())) // num_owners
+    owner_list = [i for i in range(num_owners)]
+    owner_ind = 0
+
+    for group_i in sort_len_dict.keys():
+        while len(owner_node_ids[owner_list[owner_ind]]) >= owner_nodes_len:
+            owner_list.remove(owner_list[owner_ind])
+            owner_ind = owner_ind % len(owner_list)
+        while len(owner_node_ids[owner_list[owner_ind]]) + len(partition_groups[group_i]) >= owner_nodes_len + delta:
+            owner_ind = (owner_ind + 1) % len(owner_list)
+        owner_node_ids[owner_list[owner_ind]] += partition_groups[group_i]
+
+    for owner_i in owner_node_ids.keys():
+        print('nodes len for ' + str(owner_i) + ' = ' + str(len(owner_node_ids[owner_i])))
+
+    #Count number of lost edges due to partitioning
+    num_edges = sum([len(list(whole_graph.subgraph(owner_node_ids[owner_i]).edges)) for owner_i in range(num_owners)])
+
+    return len(list(edges)) - num_edges, owner_node_ids
+
 
 class LargestConnectedComponents(BaseTransform):
     r"""Selects the subgraph that corresponds to the
