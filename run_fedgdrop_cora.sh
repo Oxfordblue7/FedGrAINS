4f191bcd6921e809a900af2d1fb0a816a86137bd
emirceyani <emirceyani@gmail.com>
2024-02-04 21:28:28 -0800

    Upload config files

--- run_fedgdrop_cora.sh
+++ run_fedgdrop_cora.sh
@@ -1,21 +1,34 @@
-#!/bin/bash
 NCLI=$1
 EXP=$2
 BATCH=$3
 LAP=$4
-CFILE=$5
-
+LOCGFN=$5
+CFILE=$6
 
 if [ $LAP -eq 0 ]; then
     echo "Disjoint"
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'Cora' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg --num_clients $NCLI  --exp_num $EXP -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'Cora' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --num_clients $NCLI  --exp_num $EXP -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'Cora' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --num_clients $NCLI --exp_num $EXP -c $CFILE
+    if [ $LOCGFN -eq 0 ]; then
+        echo "Fed GFN"
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=4 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'Cora' --seed 1 --batch_size $BATCH  --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=4  python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'Cora' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=4 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'Cora' --seed 3 --batch_size $BATCH  --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+    else
+        echo "Local GFN"
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'Cora' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'Cora' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'Cora' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --exp_num $EXP --num_clients $NCLI -c $CFILE
+    fi
 else
     echo "Overlapping"
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'Cora' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg --overlap --num_clients $NCLI  --exp_num $EXP -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'Cora' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --overlap --num_clients $NCLI  --exp_num $EXP -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'Cora' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --overlap --num_clients $NCLI --exp_num $EXP -c $CFILE
+    if [ $LOCGFN -eq 0 ]; then
+        echo "Fed GFN"
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'Cora' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg --overlap --num_clients $NCLI  --exp_num $EXP -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'Cora' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --overlap --num_clients $NCLI  --exp_num $EXP -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=3  python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'Cora' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --overlap --num_clients $NCLI --exp_num $EXP -c $CFILE
+    else
+        echo "Local GFN"
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'Cora' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --overlap --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'Cora' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --overlap --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=5  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'Cora' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --overlap --exp_num $EXP --num_clients $NCLI -c $CFILE
+    fi
 fi
-
-
--- run_fedgdrop_pubmed.sh
+++ run_fedgdrop_pubmed.sh
@@ -2,16 +2,32 @@ NCLI=$1
 EXP=$2
 BATCH=$3
 LAP=$4
-CFILE=$5
+LOCGFN=$5
+CFILE=$6
 
 if [ $LAP -eq 0 ]; then
     echo "Disjoint"
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'PubMed' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg  --exp_num $EXP --num_clients $NCLI -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'PubMed' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'PubMed' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+    if [ $LOCGFN  -eq 0 ]; then
+        echo "Fed GFN"
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'PubMed' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg  --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'PubMed' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'PubMed' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+    else
+        echo "Local GFN"
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'PubMed' --seed 1 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'PubMed' --seed 2 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'PubMed' --seed 3 --batch_size $BATCH --model GCNv2 --algo fedavg --local_flow --exp_num $EXP --num_clients $NCLI -c $CFILE
+    fi
 else
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'PubMed' --seed 1 --overlap --batch_size $BATCH --model GCNv2 --algo fedavg  --exp_num $EXP --num_clients $NCLI -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'PubMed' --seed 2 --overlap --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
-    OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'PubMed' --seed 3 --overlap --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+    if [ $LOCGFN  -eq 0 ]; then
+        echo "Fed GFN"
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'PubMed' --seed 1 --overlap --batch_size $BATCH --model GCNv2 --algo fedavg  --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'PubMed' --seed 2 --overlap --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'PubMed' --seed 3 --overlap --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+    else
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 1 --dataset 'PubMed' --seed 1 --overlap --local_flow --batch_size $BATCH --model GCNv2 --algo fedavg  --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 2 --dataset 'PubMed' --seed 2 --overlap --local_flow --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+        OMP_NUM_THREADS=10  CUDA_VISIBLE_DEVICES=6 python exps/main_fedgdrop_oneDS.py --repeat 3 --dataset 'PubMed' --seed 3 --overlap --local_flow --batch_size $BATCH --model GCNv2 --algo fedavg --exp_num $EXP --num_clients $NCLI -c $CFILE
+    fi
 fi
 
